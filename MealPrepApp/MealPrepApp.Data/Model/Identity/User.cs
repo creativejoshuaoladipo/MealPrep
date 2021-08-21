a43b3ecb6573ea4ef60fe4dbe7df5537e0ac44af
Joshua Oladipo <joshuaoladipo92@gmail.com>
2021-08-21 01:24:32 +0100

    Added IdentityRole and IdentityUser classes for authorization and authentication

---
+++ MealPrepApp/MealPrepApp.Data/Model/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealPrepApp.MealPrepApp.Data.Model.Identity
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
