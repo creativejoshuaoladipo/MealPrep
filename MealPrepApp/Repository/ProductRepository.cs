b9a85a09c10427ecc9e2fce0c0e84b070e959664
Joshua Oladipo <joshuaoladipo92@gmail.com>
2021-08-27 02:45:45 +0100

    Added the Implentation of Product Repository's Methods

--- MealPrepApp/Repository/ProductRepository.cs
+++ MealPrepApp/Repository/ProductRepository.cs
@@ -1,4 +1,8 @@
-﻿using MealPrepApp.DTOs;
+﻿using AutoMapper;
+using MealPrepApp.Data.DataContext;
+using MealPrepApp.Data.Models.Domain;
+using MealPrepApp.DTOs;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +12,62 @@ namespace MealPrepApp.Repository
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        private readonly MealPrepDBContext _dbContext;
+        private IMapper _map;
+
+        public ProductRepository(MealPrepDBContext dbContext, IMapper map)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+            _map = map;
+        }
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            var productFromDB = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == productDto.Id);
+
+            Product createdProduct = _map.Map<ProductDto, Product>(productDto);
+
+            if (productFromDB == null)
+            {
+
+                _dbContext.Products.Add(createdProduct);
+            }
+            else
+            {
+                _dbContext.Products.Update(createdProduct);
+
+            }
+
+           await _dbContext.SaveChangesAsync();
+            return  _map.Map<Product, ProductDto>(createdProduct);
         }
 
-        public Task<bool> DeleteProduct(int productId)
+        public async Task<bool> DeleteProduct(int productId)
         {
-            throw new NotImplementedException();
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == productId);
+
+            if(product == null)
+            {
+                return false;
+            } 
+            
+            _dbContext.Products.Remove(product);
+           await _dbContext.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<ProductDto> GetProductById(int productId)
+        public async Task<ProductDto> GetProductById(int productId)
         {
-            throw new NotImplementedException();
+            var products = await _dbContext.Products.FirstOrDefaultAsync(p=> p.Id == productId );
+            var productDto = _map.Map<ProductDto>(products);
+            return productDto;
         }
 
-        public Task<IEnumerable<ProductDto>> GetProducts()
+        public async Task<IEnumerable<ProductDto>> GetProducts()
         {
-            throw new NotImplementedException();
+            var products = await _dbContext.Products.ToListAsync();
+           var productListDto = _map.Map<IEnumerable<ProductDto>> (products);
+            return productListDto;
         }
     }
 }
